c2036714ee33e415ec9269e65bdfc306ddbb40ed
smhe00 <smhe00@163.com>
2026-08-16 11:04:38 +0800

    feat: export v0.4 coordination APIs

--- src/qmt_execution_core/__init__.py
+++ src/qmt_execution_core/__init__.py
@@ -1,5 +1,16 @@
 """Reusable fail-closed trading execution core."""
 
+from .coordinated_session import CoordinatedExecutionSession
+from .coordination import (
+    CashRequirementEstimate,
+    CashRequirementEstimator,
+    CashReservation,
+    ConservativeCashRequirementEstimator,
+    ExecutionCoordinator,
+    SQLiteExecutionCoordinator,
+    SymbolClaim,
+    account_key_from_binding_identity,
+)
 from .domain import (
     BrokerAsset,
     BrokerOrder,
@@ -17,29 +28,45 @@ from .domain import (
     TradeState,
 )
 from .event_queue import EventQueueState, SerialEventQueue
+from .finality import ExecutionFinality, execution_finality
 from .guards import ExecutionLimits, LimitExecutionGuard
+from .ports import AccountResourcePort, BrokerPort, ExecutionGuard
 from .session import ExecutionSession
 from .verifier import verify_state_machine
 
 __all__ = [
+    "AccountResourcePort",
     "BrokerAsset",
     "BrokerOrder",
     "BrokerOrderStatus",
+    "BrokerPort",
     "BrokerPosition",
     "BrokerTrade",
     "CancelRequestResult",
+    "CashRequirementEstimate",
+    "CashRequirementEstimator",
+    "CashReservation",
+    "ConservativeCashRequirementEstimator",
+    "CoordinatedExecutionSession",
     "EventQueueState",
+    "ExecutionCoordinator",
+    "ExecutionFinality",
+    "ExecutionGuard",
     "ExecutionLimits",
     "ExecutionRequest",
     "ExecutionSession",
     "ExecutionSnapshot",
     "LimitExecutionGuard",
     "PrecheckEvidence",
+    "SQLiteExecutionCoordinator",
     "SafetyFacts",
     "SerialEventQueue",
     "SessionEvidence",
     "Side",
+    "SymbolClaim",
     "TradeEvent",
     "TradeState",
+    "account_key_from_binding_identity",
+    "execution_finality",
     "verify_state_machine",
 ]
